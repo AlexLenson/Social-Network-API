4fffd78898e0f06dc684a293c3976103b8bcbea8
Alex Lenson <144947695+AlexLenson@users.noreply.github.com>
2023-11-24 18:44:25 -0500

    get all users

---
+++ controllers/user-controller.js
@@ -0,0 +1,15 @@
+const { User, Thought } = require("../models");
+
+const userController = {
+    async getUsers(req, res) {
+        try {
+            const users = await User.find();
+            res.json(users);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    
+}
+
+module.exports = userController;
