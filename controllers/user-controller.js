66648b2318989f1c8c5f5cb578e3d33ee180d321
Alex Lenson <144947695+AlexLenson@users.noreply.github.com>
2023-11-24 19:18:56 -0500

    add and remove friend

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -53,7 +53,36 @@ const userController = {
             res.status(500).json(err);
         }
     },
-    
+    async addFriend(req, res) {
+        try {
+            const user = await User.findOneAndUpdate(
+                { _id: req.params.userId },
+                { $addToSet: { friends: req.params.friendId } },
+                { new: true }
+            )
+            if (!user) {
+                res.status(404).json({ message: "no user found with this id" })
+            }
+            res.json(user)
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    async removeFriend(req, res) {
+        try {
+            const user = await User.findOneAndUpdate(
+                { _id: req.params.userId },
+                { $pull: { friends: req.params.friendId } },
+                { new: true }
+            )
+            if (!user) {
+                res.status(404).json({ message: "no user found with this id" })
+            }
+            res.json(user)
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    }
 }
 
 module.exports = userController;
